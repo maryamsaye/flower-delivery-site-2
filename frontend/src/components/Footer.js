569684b0bf76267a27918f333f9fb1f58bdca274
maryamsaye <marynsaye@gmail.com>
2025-05-20 17:13:34 +0100

    Move frontend folder to project root for Render deployment

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>
+        Flower Delivery App Created for ElevateHER Innovation Space Ltd By <strong>Maryam Abdu Saye</strong>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
