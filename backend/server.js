cccd0cb20173fa7a1fa6400924de15bcc13ec6b8
maryamsaye <marynsaye@gmail.com>
2025-05-23 18:21:05 +0100

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// ✅ MongoDB Connection
+
 const MONGO_URI = process.env.MONGO_URI;
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
