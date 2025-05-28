0db062826a2cdc7a850e56003ddc0ae0a1604ea2
maryamsaye <marynsaye@gmail.com>
2025-05-28 15:14:40 +0100

    updated addflower.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,7 +5,9 @@ const Footer = () => {
   return (
     <footer className="footer">
       <p>
-        Flower Delivery App Created for ElevateHER Innovation Space Ltd By <strong>Maryam Abdu Saye</strong>
+        Flower Delivery App<br />
+        Created for ElevateHER Innovation Space Ltd<br />
+        By <strong>Maryam Abdu Saye</strong>
       </p>
     </footer>
   );
